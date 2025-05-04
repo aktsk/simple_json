8f25c1b265f236636a3836eeedce40dea1260345
fumihumi <t.fum1hum@gmail.com>
2025-05-04 22:17:11 +0900

    Fix the broken bin/benchmark.sh

--- test/dummy_app/app/controllers/benchmarks_controller.rb
+++ test/dummy_app/app/controllers/benchmarks_controller.rb
@@ -8,14 +8,28 @@ class BenchmarksController < ApplicationController
 
     jb = render_to_string 'index_jb'
     jbuilder = render_to_string 'index_jbuilder'
-    simple_json = render_to_string 'index_simple_json'
+
+    SimpleJson.json_module = SimpleJson::Json::Oj
+    simple_json = render_to_string 'index'
+
+    SimpleJson.json_module = ActiveSupport::JSON
+    simple_json_active_support_json = render_to_string 'index'
+
     raise 'jb != jbuilder' unless jb == jbuilder
     raise 'simple_json != jbuilder' unless simple_json == jbuilder
+    raise 'simple_json_active_support_json != jbuilder' unless simple_json_active_support_json == jbuilder
 
     result = Benchmark.ips do |x|
       x.report('jb') { render_to_string 'index_jb' }
       x.report('jbuilder') { render_to_string 'index_jbuilder' }
-      x.report('simple_json') { render_to_string 'index_simple_json' }
+      x.report('simple_json(oj)') {
+        SimpleJson.json_module = SimpleJson::Json::Oj
+        render_to_string 'index'
+      }
+      x.report('simple_json(AS::json)') {
+        SimpleJson.json_module = ActiveSupport::JSON
+        render_to_string 'index'
+      }
       x.compare!
     end
     render plain: result.data.to_s
